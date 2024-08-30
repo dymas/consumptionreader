97bc1dd18b47af29e8de972f04fd6fe938f73477
Dymas <dymassousa@gmail.com>
2024-08-30 17:33:09 -0300

    feat: implement confirmMeasurement

--- src/usecases/confirmMeasurement.ts
+++ src/usecases/confirmMeasurement.ts
@@ -1,3 +1,21 @@
-export async function confirmMeasurement(measure_uuid: string, confirmed_value) {
-  return true;
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function confirmMeasurement(measure_uuid: string) {
+  try {
+    const updatedMeasurement = await prisma.measurement.update({
+      where: {
+        measure_uuid: measure_uuid,
+      },
+      data: {
+        has_confirmed: true,
+      },
+    });
+
+    return updatedMeasurement;
+  } catch (error) {
+    console.error("Erro ao confirmar a medição:", error);
+    throw new Error("Erro ao confirmar a medição");
+  }
 }
