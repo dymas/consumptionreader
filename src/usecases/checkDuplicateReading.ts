f6f6a6c672e43ada0c98eed3eaff823757ec5daa
Dymas <dymassousa@gmail.com>
2024-08-30 17:34:08 -0300

    feat: implement checkDuplicateReading

--- src/usecases/checkDuplicateReading.ts
+++ src/usecases/checkDuplicateReading.ts
@@ -1,7 +1,25 @@
-export function checkDuplicateReading(
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function checkDuplicateReading(
   customer_code: string,
   measure_type: string,
   measure_datetime: string
-): boolean {
-  return false;
+) {
+  try {
+    const measureDate = new Date(measure_datetime);
+    const duplicateMeasurement = await prisma.measurement.findFirst({
+      where: {
+        customer_code: customer_code,
+        measure_type: measure_type,
+        measure_datetime: measureDate,
+      },
+    });
+
+    return duplicateMeasurement !== null;
+  } catch (error) {
+    console.error("Erro ao verificar leitura duplicada:", error);
+    throw new Error("Erro ao verificar leitura duplicada");
+  }
 }
