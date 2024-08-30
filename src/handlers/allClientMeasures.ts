89819ddbeee123b78a261c29b0c524ce3eaae55b
Dymas <dymassousa@gmail.com>
2024-08-30 15:36:12 -0300

    feat: add allClientMeasures handler

---
+++ src/handlers/allClientMeasures.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+import { getMeasurementsByCustomerCode } from "#usecases/getMeasurementsByCustomerCode";
+
+export async function allClientMeasuresHandler(req: Request, res: Response) {
+  const customer_code = req.params.customer_code;
+  const measure_type = req.query.measure_type as string | undefined;
+  const valid_measure_types = ["WATER", "GAS"];
+  const is_valid_measure_type = valid_measure_types.includes(
+    measure_type.toUpperCase()
+  );
+
+  if (!is_valid_measure_type) {
+    return res.status(400).json({
+      error_code: "INVALID_TYPE",
+      error_description: "Tipo de medição não permitida",
+    });
+  }
+
+  try {
+    const measures = await getMeasurementsByCustomerCode(customer_code, [
+      measure_type.toUpperCase(),
+    ]);
+
+    if (!measures) {
+      return res.status(404).json({
+        error_code: "MEASURES_NOT_FOUND",
+        error_description: "Nenhuma leitura encontrada",
+      });
+    }
+
+    return res.status(200).json({
+      customer_code: customer_code,
+      measures: measures,
+    });
+  } catch (error) {
+    console.error("Erro ao buscar medições:", error);
+    return res.status(500).json({
+      error_code: "SERVER_ERROR",
+      error_description:
+        "Erro ao processar a requisição. Tente novamente mais tarde.",
+    });
+  }
+}
