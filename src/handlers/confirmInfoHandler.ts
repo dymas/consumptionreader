38720176f6f17e370d1779fbd9090e7efbf41236
Dymas <dymassousa@gmail.com>
2024-08-30 13:45:49 -0300

    style: fix identation in confirmInfoHandler

--- src/handlers/confirmInfoHandler.ts
+++ src/handlers/confirmInfoHandler.ts
@@ -5,7 +5,9 @@ import { confirmMeasurement } from "#usecases/confirmMeasurement";
 
 const confirmInfoRequestBody = z.object({
   measure_uuid: z.string().uuid("O código de leitura deve ser um UUID válido."),
-  confirmed_value: z.number().int("O valor confirmado deve ser um número inteiro.")
+  confirmed_value: z
+    .number()
+    .int("O valor confirmado deve ser um número inteiro."),
 });
 
 export async function confirmInfoHandler(req: Request, res: Response) {
@@ -22,30 +24,30 @@ export async function confirmInfoHandler(req: Request, res: Response) {
 
   try {
     const measurement = await getMeasurementByUuid(measure_uuid);
-    
+
     if (!measurement) {
       return res.status(404).json({
         error_code: "MEASURE_NOT_FOUND",
-        error_description: "Código de leitura não encontrado."
+        error_description: "Código de leitura não encontrado.",
       });
     }
 
     if (measurement.has_confirmed) {
       return res.status(409).json({
         error_code: "CONFIRMATION_DUPLICATE",
-        error_description: "Leitura já confirmada."
+        error_description: "Leitura já confirmada.",
       });
     }
 
     await confirmMeasurement(measure_uuid, confirmed_value);
 
     return res.status(200).json({ success: true });
-
   } catch (error) {
     console.error("Erro ao confirmar a leitura:", error);
     return res.status(500).json({
       error_code: "SERVER_ERROR",
-      error_description: "Erro ao processar a requisição. Tente novamente mais tarde."
+      error_description:
+        "Erro ao processar a requisição. Tente novamente mais tarde.",
     });
   }
 }
